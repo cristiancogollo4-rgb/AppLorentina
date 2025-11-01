864e5cb7ace9fe549498d7ba3d2c44682a0707b4
Oscar Sanchez <ds4219759@gmail.com>
2025-11-01 14:19:33 -0500

    Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,6 +57,7 @@ dependencies {
     implementation(libs.material3)
     implementation(libs.foundation)
     implementation(libs.androidx.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +68,9 @@ dependencies {
     // ASEGÚRATE DE QUE ESTA LÍNEA ESTÉ PRESENTE:
     implementation("androidx.compose.material:material-icons-extended")
 
+    //Firebase
+    implementation(libs.firebase.bom)
+
+    //auth
+    implementation(libs.firebase.auth)
 }
